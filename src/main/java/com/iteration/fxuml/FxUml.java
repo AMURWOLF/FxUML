a23c2509a8139c0e8dbee2c22c8e18d11d5c01dd
AMURWOLF <amurwolf@lenta.ru>
2018-10-02 10:46:42 +1000

    Project 'FxUML created

---
+++ src/main/java/com/iteration/fxuml/FxUml.java
@@ -0,0 +1,54 @@
+/***************************************************************************************************
+ * Copyright (C) 2018 Iteration
+ * License for sources distribution: see file 'LICENSE' in the project's root
+ **************************************************************************************************/
+
+
+package com.iteration.fxuml;
+
+
+
+/**
+ * Main class of the application 'FxUML'.
+ * 
+ * @author AMURWOLF (Dmitry Malkov)
+ * @since October 2018
+ */
+public class FxUml {
+    
+    ///////////////////////////
+    ////    F I E L D S    ////
+    ///////////////////////////
+    
+    ////////////////////////////////////////////
+    ////    S T A T I C    M E T H O D S    ////
+    ////////////////////////////////////////////
+    
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application. main() serves only as
+     * fallback in case the application can not be launched through deployment artifacts, e.g., in
+     * IDEs with limited FX support.
+     * 
+     * @param args
+     *            the command line arguments
+     * 
+     * @author AMURWOLF (Dmitry Malkov)
+     * @since October 2018
+     */
+    public static void main(String[] args) {
+        System.out.println("FxUml started");
+    }
+    
+    ///////////////////////////////////////
+    ////    C O N S T R U C T O R S    ////
+    ///////////////////////////////////////
+    
+    /////////////////////////////
+    ////    M E T H O D S    ////
+    /////////////////////////////
+    
+    /////////////////////////
+    ////    T Y P E S    ////
+    /////////////////////////
+    
+}
---
+++ src/main/java/com/iteration/fxuml/package-info.java
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * Copyright (C) 2018 Iteration
+ * License for sources distribution: see file 'LICENSE' in the project's root
+ **************************************************************************************************/
+
+/**
+ * The package contains all the application 'FxUML'
+ * 
+ * @author AMURWOLF (Dmitry Malkov)
+ * @since October 2018
+ */
+
+
+package com.iteration.fxuml;
